510ddfcfe9422ea0e14292fb1b6b3abe640a8b47
olegtemek <olegtemek@gmail.com>
2024-10-06 03:01:38 +0500

    2024-10-06_03-01-38-backup

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,18 +1,16 @@
-
- return {
-	 "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-	 config = function()
-		
-  require("neo-tree").setup({
-    close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
-    popup_border_style = "rounded",
-    enable_git_status = true,
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+      popup_border_style = "rounded",
+      enable_git_status = true,
       filesystem = {
         filtered_items = {
           visible = true,
@@ -20,16 +18,24 @@
           hide_gitignored = true
         }
       },
-    window = {
-      mappings = {
-        ["<cr>"] = "open",
-        ["<2-LeftMouse>"] = "open",
-        ["l"] = "open",
+      window = {
+        mappings = {
+          ["<cr>"] = "open",
+          ["<2-LeftMouse>"] = "open",
+          ["l"] = "open",
+        }
+      },
+      event_handlers = {
+        {
+          event = "file_open_requested",
+          handler = function()
+            vim.cmd("Neotree close")
+          end
+        },
+
       }
-    }
-})
-     vim.keymap.set("n", "<leader>e", ":Neotree float toggle<CR>", { desc = "NeoTree: Open Explorer float" })
+    })
+    vim.keymap.set("n", "<leader>e", ":Neotree float toggle<CR>", { desc = "NeoTree: Open Explorer float" })
+  end
 
-	 end
-
- }
+}
--- nvim/lua/plugins/startup.lua
+++ nvim/lua/plugins/startup.lua
@@ -20,44 +20,44 @@ return {
         },
         center = {
           {
-            icon = ' ',
+            icon = '',
             icon_hl = 'Title',
             desc = 'Open NeoTree          ',
             desc_hl = 'String',
-            key = 'b',
+            key = ' ',
             keymap = 'SPC e',
             key_hl = 'Number',
             key_format = ' %s', -- remove default surrounding `[]`
             action = 'lua print(2)'
           },
           {
-            icon = ' ',
+            icon = '',
             icon_hl = 'Title',
             desc = 'File File Telescope    ',
             desc_hl = 'String',
-            key = 'c',
+            key = ' ',
             keymap = 'SPC f f',
             key_hl = 'Number',
             key_format = ' %s', -- remove default surrounding `[]`
             action = 'lua print(2)'
           },
           {
-            icon = ' ',
+            icon = '',
             icon_hl = 'Title',
             desc = 'File Word Telescope    ',
             desc_hl = 'String',
-            key = 'd',
+            key = ' ',
             keymap = 'SPC f s',
             key_hl = 'Number',
             key_format = ' %s', -- remove default surrounding `[]`
             action = 'lua print(2)'
           },
           {
-            icon = ' ',
+            icon = '',
             icon_hl = 'Title',
             desc = "File Todo's Telescope    ",
             desc_hl = 'String',
-            key = 'e',
+            key = ' ',
             keymap = 'SPC f t',
             key_hl = 'Number',
             key_format = ' %s', -- remove default surrounding `[]`
