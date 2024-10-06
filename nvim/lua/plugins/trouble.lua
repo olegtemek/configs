51ee87ef409eedb507ffaa9a8965e8d1278558de
olegtemek <olegtemek@gmail.com>
2024-10-06 17:29:32 +0500

    2024-10-06_17-29-32-backup

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -6,7 +6,7 @@ return {
   },
   cmd = "Trouble",
   keys = {
-    { "<leader>fe", "<cmd>Trouble diagnostics toggle<CR>", desc = "Trouble: Open trouble workspace diagnostics" },
+    { "<leader>ge", "<cmd>Trouble diagnostics toggle<CR>", desc = "Trouble: Open trouble workspace diagnostics" },
     -- { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Trouble: Open trouble document diagnostics" },
     -- { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>",                 desc = "Trouble: Open trouble quickfix list" },
     -- { "<leader>xl", "<cmd>Trouble loclist toggle<CR>",                  desc = "Trouble: Open trouble location list" },
